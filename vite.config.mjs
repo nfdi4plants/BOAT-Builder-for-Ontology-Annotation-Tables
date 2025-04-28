36e3632cb30d97ae31d1d4358aaf80f4232fbd8e
Rookabu <annika-paul1@gmx.de>
2025-04-28 16:28:40 +0200

    change vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   root: "./src",
+  base: "./",
   build: {
     outDir: "../dist",
   }
